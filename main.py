92d76d6b402d3ecac51a6e01cfd1a1a9b5bde793
ns3838 <72807027+ns3838@users.noreply.github.com>
2021-01-21 23:48:42 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,2 @@
+music.set_volume(20)
+music.play_melody("A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F ", 250)
---
+++ main.ts
@@ -0,0 +1,2 @@
+music.setVolume(20)
+music.playMelody("A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F A C G F ", 250)
